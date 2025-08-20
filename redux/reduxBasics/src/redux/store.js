36e48924a75fa7bf4f76c70c0c89b3186c5f110d
ZenabShaik <shaikzenab07@gmail.com>
2025-08-20 10:18:54 +0530

    Update store.js

--- redux/reduxBasics/src/redux/store.js
+++ redux/reduxBasics/src/redux/store.js
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice.jsx";
 import userSlice from "./userSlice.jsx";
 
-import thunkMiddleware from "redux-thunk";
+//import thunkMiddleware from "redux-thunk";
 const store = configureStore({
   reducer: {  
     counterState : counterSlice.reducer,
     userState: userSlice.reducer,
   },
-  middleware:[thunkMiddleware]
+  //middleware:[thunkMiddleware]
 })
 
 export default store;
